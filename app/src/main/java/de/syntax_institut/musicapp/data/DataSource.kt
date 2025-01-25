f1809dafc5921d38a775aa4d4d32278b7826f858
Andre Eggenschwiler <swissi1984@gmail.com>
2025-01-25 14:51:54 +0100

    Aufgaben Freitag

--- app/src/main/java/de/syntax_institut/musicapp/data/DataSource.kt
+++ app/src/main/java/de/syntax_institut/musicapp/data/DataSource.kt
@@ -8,6 +8,18 @@ val songList = listOf(
     Song("Queen", "Bohemian Rhapsody", 354, R.drawable.queen, R.raw.queen),
     Song("Michael Jackson", "Thriller", 357, R.drawable.jackson, R.raw.thriller),
     Song("Adele", "Rolling in the Deep", 228, R.drawable.adele,R.raw.adele),
+    Song("The Beatles", "Hey Jude", 431, R.drawable.beatles, R.raw.jude),
+    Song("Queen", "Bohemian Rhapsody", 354, R.drawable.queen, R.raw.queen),
+    Song("Michael Jackson", "Thriller", 357, R.drawable.jackson, R.raw.thriller),
+    Song("Adele", "Rolling in the Deep", 228, R.drawable.adele,R.raw.adele),
+    Song("The Beatles", "Hey Jude", 431, R.drawable.beatles, R.raw.jude),
+    Song("Queen", "Bohemian Rhapsody", 354, R.drawable.queen, R.raw.queen),
+    Song("Michael Jackson", "Thriller", 357, R.drawable.jackson, R.raw.thriller),
+    Song("Adele", "Rolling in the Deep", 228, R.drawable.adele,R.raw.adele),
+    Song("The Beatles", "Hey Jude", 431, R.drawable.beatles, R.raw.jude),
+    Song("Queen", "Bohemian Rhapsody", 354, R.drawable.queen, R.raw.queen),
+    Song("Michael Jackson", "Thriller", 357, R.drawable.jackson, R.raw.thriller),
+    Song("Adele", "Rolling in the Deep", 228, R.drawable.adele,R.raw.adele),
     )
 
 
